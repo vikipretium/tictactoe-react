355e7eeef295c21c750a7dc45b1293a56a98e4f0
vikipretium <vighneshpasupathy@gmail.com>
2023-03-01 09:08:04 +0530

    move handleclick to game class

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O"
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
-  }
   renderSquare(i) {
     return (
       <Square
@@ -27,7 +19,6 @@ class Board extends React.Component {
       />
     )
   }
-
   render() {
     return (
       <div>
@@ -63,6 +54,21 @@ class Game extends React.Component {
       xIsNext: true,
     }
   }
+
+  handleClick(i) {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
   render() {
     const history = this.state.history
     const current = history[history.length - 1]
