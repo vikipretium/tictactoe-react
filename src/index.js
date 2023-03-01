ccba2780a5de117613607e61fc675f2c70a0e89f
vikipretium <vighneshpasupathy@gmail.com>
2023-03-01 07:08:34 +0530

    add calculate winner func

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? "X" : "O"
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
   }
@@ -33,7 +36,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O")
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = "Winner: " + winner
+    } else {
+      status = "Next Player: " + this.state.xIsNext ? "X" : "O"
+    }
 
     return (
       <div>
